cf49c6a33d4738d6130a6f4cbfeb39408e149cbe
Martín Morondo <102574694+martinmorondo@users.noreply.github.com>
2023-05-30 01:45:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const imagenes = [
-    'imagen-0', 'imagen-1', 'imagen-2',
-    'imagen-3', 'imagen-4', 'imagen-5',
-    'imagen-6', 'imagen-7', 'imagen-8'
+  'imagen-0', 'imagen-1', 'imagen-2',
+  'imagen-3', 'imagen-4', 'imagen-5',
+  'imagen-6', 'imagen-7', 'imagen-8'
 ];
 
-const puzzle = document.getElementById('puzzle')
-const piezas = document.getElementById('piezas')
-const mensaje = document.getElementById('mensaje')
+const puzzle = document.getElementById('puzzle');
+const piezas = document.getElementById('piezas');
+const mensaje = document.getElementById('mensaje');
+let terminado = imagenes.length;
 
-let terminado = imagenes.length
-
-while (imagenes.length) {
+function generarPiezas() {
+  while (imagenes.length) {
     const index = Math.floor(Math.random() * imagenes.length);
     const div = document.createElement('div');
     div.className = 'pieza';
@@ -19,41 +19,53 @@ while (imagenes.length) {
     div.style.backgroundImage = `url("imagenes/${imagenes[index]}.jpg")`;
     piezas.appendChild(div);
     imagenes.splice(index, 1);
+  }
 }
 
-for (let i = 0; i < terminado; i++) {
+function crearPlaceholders() {
+  for (let i = 0; i < terminado; i++) {
     const div = document.createElement('div');
     div.className = 'placeholder';
     div.dataset.id = i;
     puzzle.appendChild(div);
+  }
 }
 
-piezas.addEventListener('dragstart', e => {
+function iniciarArrastrar() {
+  piezas.addEventListener('dragstart', e => {
     e.dataTransfer.setData('id', e.target.id);
-});
+  });
+}
 
-puzzle.addEventListener('dragover', e => {
+function manejarArrastrarSoltar() {
+  puzzle.addEventListener('dragover', e => {
     e.preventDefault();
     e.target.classList.add('hover');
-});
+  });
 
-puzzle.addEventListener('dragleave', e => {
+  puzzle.addEventListener('dragleave', e => {
     e.target.classList.remove('hover');
-});
+  });
 
-puzzle.addEventListener('drop', e => {
+  puzzle.addEventListener('drop', e => {
     e.target.classList.remove('hover');
 
     const id = e.dataTransfer.getData('id');
     const numero = id.split('-')[1];
 
     if (e.target.dataset.id === numero) {
-        e.target.appendChild(document.getElementById(id));
+      e.target.appendChild(document.getElementById(id));
 
-        terminado--;
+      terminado--;
 
-        if (terminado === 0) {
-            document.body.classList.add('ganaste');
-        }
+      if (terminado === 0) {
+        document.body.classList.add('ganaste');
+      }
     }
-});
+  });
+}
+
+generarPiezas();
+crearPlaceholders();
+iniciarArrastrar();
+manejarArrastrarSoltar();
